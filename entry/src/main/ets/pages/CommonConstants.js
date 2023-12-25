af4361912b25e1f387af047b7e34d75059d19468
MagicianZC <18850317627@163.com>
2023-12-25 18:02:14 +0800

    首页

---
+++ entry/src/main/ets/pages/CommonConstants.js
@@ -0,0 +1,58 @@
+/**
+ * 所有功能的通用常量。
+ */
+export default class CommonConstants {
+}
+/**
+ * 账号的输入长度。
+ */
+CommonConstants.INPUT_ACCOUNT_LENGTH = 11;
+/**
+ * 密码的输入长度。
+ */
+CommonConstants.INPUT_PASSWORD_LENGTH = 8;
+/**
+ * 输入框的左填充。
+ */
+CommonConstants.INPUT_PADDING_LEFT = 0;
+/**
+ * 模拟登录的延迟时间。
+ */
+CommonConstants.LOGIN_DELAY_TIME = 2000;
+/**
+ * 组件的通用间距。
+ */
+CommonConstants.COMMON_SPACE = 12;
+/**
+ * 首页的标题文本。
+ */
+CommonConstants.HOME_TITLE = '首页';
+/**
+ * 设置页面的标题文本。
+ */
+CommonConstants.MINE_TITLE = '我的';
+/**
+ * 其他登录方法的间距。
+ */
+CommonConstants.LOGIN_METHODS_SPACE = 44;
+/**
+ * 组件的宽度或高度覆盖父组件。
+ */
+CommonConstants.FULL_PARENT = '100%';
+/**
+ * 按钮的宽度。
+ */
+CommonConstants.BUTTON_WIDTH = '90%';
+/**
+ * 设置列表的宽度。
+ */
+CommonConstants.SET_LIST_WIDTH = '42%';
+/**
+ * 首页标签索引。
+ */
+CommonConstants.HOME_TAB_INDEX = 0;
+/**
+ * 我的设置也没标签索引。
+ */
+CommonConstants.MINE_TAB_INDEX = 1;
+//# sourceMappingURL=CommonConstants.js.map
---
+++ entry/src/main/ets/pages/ItemData.js
@@ -0,0 +1,8 @@
+export default class ItemData {
+    constructor(title, img, others) {
+        this.title = title;
+        this.img = img;
+        this.others = others;
+    }
+}
+//# sourceMappingURL=ItemData.js.map
